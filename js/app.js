8c9fc01dd1a9d2ffc696f102d03cbda355d162d7
Ileana Argyris <argyris.ileana@gmail.com>
2018-06-26 16:51:52 +0200

    Instantiated objects

--- js/app.js
+++ js/app.js
@@ -25,26 +25,30 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var player = function() {
-    this.sprite = 'images/char-boy.png';
-}
+var Player = function() {
+    this.sprite = 'images/char-horn-girl.png';
+};
 
-player.update = function() {
+Player.update = function() {
 
-}
+};
 
-player.render = function() {
+Player.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
-}
+Player.handleInput = function() {
 
-player.handleInput = function() {
+};
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
 
-    }
-    // Now instantiate your objects.
-    // Place all enemy objects in an array called allEnemies
-    // Place the player object in a variable called player
+new Player();
+new Enemy();
 
 let allEnemies = [];
+let player = Player;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
